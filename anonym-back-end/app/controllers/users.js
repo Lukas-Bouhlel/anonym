470d65e17f48d84b14123487f0a1e61c28f44bc8
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-08-13 12:23:07 +0200

    Add create user from admin

--- anonym-back-end/app/controllers/users.js
+++ anonym-back-end/app/controllers/users.js
@@ -21,6 +21,53 @@ exports.readAll = async (req, res) => {
     }
 };
 
+exports.create = async (req, res) => {
+    try {
+        const { username, email, password, avatar, roles } = req.body;
+
+        // Vérifier que l'utilisateur est soit ADMIN, soit SUPER_ADMIN
+        if (req.auth.userRole === 'USER') {
+            return res.status(403).json({ message: "You do not have permission to create a user." });
+        }
+
+        if (!username) {
+            return res.status(400).json({ message: "Username are required." });
+        }
+
+        if (!email) {
+            return res.status(400).json({ message: "Email are required." });
+        }
+
+        if (!password) {
+            return res.status(400).json({ message: "Password are required." });
+        }
+
+        // Si l'utilisateur tente de créer un rôle autre que 'USER'
+        if (roles && roles !== 'USER') {
+            // Vérifier si le rôle de l'utilisateur qui fait la requête est SUPER_ADMIN
+            if (req.auth.userRole !== 'SUPER_ADMIN') {
+                return res.status(403).json({ message: "Only SUPER_ADMIN can assign roles other than USER." });
+            }
+        }
+
+        // Création de l'utilisateur avec les informations fournies
+        const newUser = await User.create({
+            username,
+            email,
+            password: await bcrypt.hash(password, 10),
+            avatar,
+            roles: roles || 'USER' // Par défaut, le rôle est USER
+        });
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || 'An error occurred while creating the user.'
+        });
+    }
+};
+
+
 exports.update = async (req, res) => {
     try {
         const userId = req.params.id;
--- anonym-back-end/app/routes/admin.js
+++ anonym-back-end/app/routes/admin.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../controllers/users.js');
 const authMiddleware = require("../middlewares/auth.js");
+const normalizeEmailMiddleware = require('../middlewares/normalizeEmail');
 
 router.get('/users', authMiddleware, usersCtrl.readAll);
-router.put('/users/:id', authMiddleware, usersCtrl.update);
+router.post('/users', authMiddleware, normalizeEmailMiddleware, usersCtrl.create);
+router.put('/users/:id', authMiddleware, normalizeEmailMiddleware, usersCtrl.update);
 router.delete('/users/:id', authMiddleware, usersCtrl.delete);
 
 module.exports = router;
