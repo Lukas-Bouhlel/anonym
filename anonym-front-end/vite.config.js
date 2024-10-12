40100322b8e49ee44906f63550cdc78cb5348433
Rei <60601546+Lukas-Bouhlel@users.noreply.github.com>
2024-10-12 02:24:32 +0200

    last test CI

--- anonym-front-end/vite.config.js
+++ anonym-front-end/vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler' // or "modern"
+        api: 'modern-compiler'
       }
     }
   },
