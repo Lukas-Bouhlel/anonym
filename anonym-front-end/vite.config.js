af5f15927f54253b995e447c877a25ef6954a0ed
Rei <60601546+Lukas-Bouhlel@users.noreply.github.com>
2024-10-20 12:07:27 +0200

    test commit

--- anonym-front-end/vite.config.js
+++ anonym-front-end/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
-import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
